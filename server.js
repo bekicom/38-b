56c6d3e476014907c3c98da06ce10422b83fbe0d
bekicom <adelkauz6@gmail.com>
2025-03-19 12:09:50 +0500

    sever

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const corsOptions = {
   origin: [
     "http://localhost:3000",
 
-    "https://38-f.vercel.app/", // Frontend
+    "https://38-f.vercel.app", // Frontend
     // Backend lokal
   ],
   methods: ["GET", "POST", "PUT", "DELETE"], // Ruxsat etilgan HTTP metodlar
